1a938458661a862aea363b4606a4b01a5b54b173
Dudarik <Dudarik@yandex.ru>
2022-12-20 14:37:52 +0500

    feat: add filds to store

--- src/assets/interfaces/store.ts
+++ src/assets/interfaces/store.ts
@@ -3,4 +3,11 @@ import { GoodsItem } from './goodsItem';
 export interface Store {
     goodsItems: GoodsItem[];
     filteredGoodsItems: GoodsItem[];
+    cart: GoodsItem[];
+    sumCartItems: number;
+    ls_key_cart: string;
+    ls_key_settings: string;
+    settings: {
+        isLSAvailabel: boolean;
+    };
 }
