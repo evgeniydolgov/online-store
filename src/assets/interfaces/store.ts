9c32152e451889c4eef97537d05bb6218f3a91bb
Dudarik <Dudarik@yandex.ru>
2022-12-22 10:32:34 +0500

    feat: refactor store, add filters settings

--- src/assets/interfaces/store.ts
+++ src/assets/interfaces/store.ts
@@ -5,6 +5,17 @@ export interface Store {
     filteredGoodsItems: GoodsItem[];
     cart: GoodsItem[];
     sumCartItems: number;
+
+    filters_settings: {
+        brands: string[];
+
+        min_price: number;
+        max_price: number;
+
+        min_count_goods: number;
+        max_count_goods: number;
+    };
+
     ls_key_cart: string;
     ls_key_settings: string;
     settings: {
--- src/assets/ts/store.ts
+++ src/assets/ts/store.ts
@@ -5,6 +5,17 @@ export const store: Store = {
     filteredGoodsItems: [],
     cart: [],
     sumCartItems: 0.0,
+
+    filters_settings: {
+        brands: [],
+
+        min_price: 0,
+        max_price: 0,
+
+        min_count_goods: 0,
+        max_count_goods: 0,
+    },
+
     ls_key_cart: 'dudarik_online_shop_cart',
     ls_key_settings: 'dudarik_online_shop_settings',
     settings: {
