1660ebaa938d969b03ce138cbae9cec6e12e2af2
Dudarik <Dudarik@yandex.ru>
2022-12-22 12:52:45 +0500

    feat: minor changes

--- src/assets/ts/lib/initStore.ts
+++ src/assets/ts/lib/initStore.ts
@@ -1,21 +1,44 @@
 import { Filter, GoodsItem } from '../../interfaces';
 import { Store } from '../../interfaces/store';
 import { goods } from '../goods/goodsArray';
-import { filterGoods, Filters } from './filterGoods';
+import { filterGoods } from './filterGoods';
+import { LS } from './localstorage';
 
 const FILTERS_NAME_ARRAY = ['_brand', '_category', 'price', 'stock'];
 
 const loadDataStore = () => goods;
 
-const getFilters = () => {
+const getFiltersFromUrl = () => {
     const url = new URL(location.href);
 
-    const filters: Filter[] = FILTERS_NAME_ARRAY.reduce((acc: Filter[], item: string) => {}, []);
+    const filters: Filter[] = FILTERS_NAME_ARRAY.reduce((acc: Filter[], item: string) => {
+        const currFilter = url.searchParams.get(item);
+
+        if (currFilter !== null) {
+            const currFilterParams = currFilter.split(',');
+            acc = [...acc, { name: item, value: currFilterParams }];
+        }
+        return acc;
+    }, []);
+
+    return filters;
 };
 
-export function initStore(goods: GoodsItem[], filters: Filters[]) {
+const getSearchStringFromUrl = () => {
+    const url = new URL(location.href);
+
+    return url.searchParams.get('search');
+};
+
+export function initStore(goods: GoodsItem[]) {
+    const cart = LS.loadCartDataFromLS() || [];
+    const sumCartItems =
+        cart.length === 0 ? 0 : cart.reduce((acc: number, item: GoodsItem) => acc + Number(item.price), 0);
+
     const store: Store = {
-        goodsItems: loadDataStore(),
-        filteredGoodsItems: filterGoods(loadDataStore()),
+        goodsItems: goods,
+        filteredGoodsItems: filterGoods(goods, getFiltersFromUrl(), getSearchStringFromUrl()),
+        cart,
+        sumCartItems,
     };
 }
