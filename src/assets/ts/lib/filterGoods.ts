d1dd7ec64d9d59cb07e866c6eb01acaff73e7268
Dudarik <Dudarik@yandex.ru>
2022-12-23 12:55:30 +0500

    feat: add filter functions, initStore function

--- src/assets/ts/lib/filterGoods.ts
+++ src/assets/ts/lib/filterGoods.ts
@@ -1,4 +1,7 @@
 import { Filter, GoodsItem } from '../../interfaces';
+import { store } from '../store';
+
+const FILTERS_NAME_ARRAY = ['_brand', '_category', 'price', 'stock'];
 
 export abstract class Filters {
     private static SEARCH_FIELDS = ['title', 'description', 'price', 'rating', 'stock', 'brand', 'category'];
@@ -60,3 +63,54 @@ export function filterGoods(goodsItems: GoodsItem[], filters: Filter[], searchSt
 
     return retArr;
 }
+
+export const getFiltersFromUrl = () => {
+    const url = new URL(location.href);
+
+    return FILTERS_NAME_ARRAY.reduce((acc: Filter[], filterName: string) => {
+        const currFilter = url.searchParams.get(filterName);
+
+        if (currFilter !== null) {
+            let currFilterParams = currFilter.split(',');
+            if (filterName[0] === '_') {
+                currFilterParams = currFilterParams.filter((param) => {
+                    return store.filters_settings[`all${filterName}`].includes(param);
+                });
+            } else {
+                currFilterParams =
+                    parseInt(currFilterParams[0]) >= parseInt(currFilterParams[1]) ? currFilterParams : [];
+            }
+            if (currFilterParams.length > 0) acc = [...acc, { name: filterName, value: currFilterParams }];
+        }
+        return acc;
+    }, []);
+};
+
+export const getSearchStringFromUrl = () => {
+    const url = new URL(location.href);
+
+    return url.searchParams.get('search');
+};
+
+export const getCountGoodsByFieldName = (goods: GoodsItem[], fieldName: string, valueField: string) =>
+    goods.filter((goodsItem) => String(goodsItem[fieldName]).toLocaleLowerCase() === valueField.toLocaleLowerCase())
+        .length;
+
+export const getMinMaxByFieldName = (goods: GoodsItem[], fieldName: string) => {
+    const retArr = [0, 0];
+
+    if (goods.length > 0) {
+        retArr[0] = parseInt(String(goods[0][fieldName]));
+        retArr[1] = parseInt(String(goods[0][fieldName]));
+
+        for (let i = 0; i < goods.length; i++) {
+            const item = parseInt(String(goods[i][fieldName]));
+
+            if (item < retArr[0]) retArr[0] = item;
+
+            if (item > retArr[1]) retArr[1] = item;
+        }
+    }
+
+    return retArr;
+};
--- src/assets/ts/lib/initStore.ts
+++ src/assets/ts/lib/initStore.ts
@@ -1,80 +1,33 @@
-import { Filter, GoodsItem } from '../../interfaces';
-import { CartItems } from '../../types';
-import { goods } from '../goods/goodsArray';
-import { filterGoods } from './filterGoods';
+import { GoodsItem } from '../../interfaces';
+import {
+    filterGoods,
+    // getCountGoodsByFieldName,
+    getFiltersFromUrl,
+    // getMinMaxByFieldName,
+    getSearchStringFromUrl,
+} from './filterGoods';
 import { LS } from './localstorage';
 import { store } from '../store';
-
-const FILTERS_NAME_ARRAY = ['_brand', '_category', 'price', 'stock'];
-
-const loadDataStore = () => goods;
-
-const getFiltersFromUrl = () => {
-    const url = new URL(location.href);
-
-    const filters: Filter[] = FILTERS_NAME_ARRAY.reduce((acc: Filter[], item: string) => {
-        const currFilter = url.searchParams.get(item);
-
-        if (currFilter !== null) {
-            const currFilterParams = currFilter.split(',');
-            acc = [...acc, { name: item, value: currFilterParams }];
-        }
-        return acc;
-    }, []);
-
-    return filters;
-};
-
-const getSearchStringFromUrl = () => {
-    const url = new URL(location.href);
-
-    return url.searchParams.get('search');
-};
-
-const getCartSum = (cart: CartItems) => {
-    const cartItems = Object.entries(cart);
-
-    const goods = loadDataStore();
-
-    return cartItems.reduce((acc, itemCart) => {
-        const goodsItem = goods.find((itemGoods) => itemGoods.id === parseFloat(itemCart[0]));
-        if (goodsItem) acc = acc + goodsItem.price * parseFloat(itemCart[1]);
-
-        return acc;
-    }, 0);
-};
-
-const getCountGoodsByFieldName = (goods: GoodsItem[], fieldName: string, valueField: string) =>
-    goods.filter((goodsItem) => String(goodsItem[fieldName]).toLocaleLowerCase() === valueField.toLocaleLowerCase())
-        .length;
-
-const getMinMaxByFieldName = (goods: GoodsItem[], fieldName: string) => {
-    const retArr = [0, 0];
-
-    if (goods.length > 0) {
-        retArr[0] = parseInt(String(goods[0][fieldName]));
-        retArr[1] = parseInt(String(goods[0][fieldName]));
-
-        for (let i = 0; i < goods.length; i++) {
-            const item = parseInt(String(goods[i][fieldName]));
-
-            if (item < retArr[0]) retArr[0] = item;
-
-            if (item > retArr[1]) retArr[1] = item;
-        }
-    }
-
-    return retArr;
-};
+import { loadDataStore } from './loadData';
+import { getCartSum } from './cartFunctions';
 
 const getGoodsBrands = (goods: GoodsItem[]) => Array.from(new Set(goods.map((goodsItem) => String(goodsItem.brand))));
 
-const getGoodsCategorys = (goods: GoodsItem[]) =>
+const getGoodsCategories = (goods: GoodsItem[]) =>
     Array.from(new Set(goods.map((goodsItem) => String(goodsItem.category))));
 
 export function initStore() {
+    store.settings.isLSAvailabel = LS.isLSAvailabel();
+
+    store.goodsItems = loadDataStore();
+
     const cart = LS.loadCartDataFromLS() || {};
-    const sumCartItems = getCartSum(cart);
+
+    store.cart = cart;
+    store.sumCartItems = getCartSum(cart);
+
+    store.filters_settings.all_brand = getGoodsBrands(store.goodsItems);
+    store.filters_settings.all_category = getGoodsCategories(store.goodsItems);
 
     const filters = getFiltersFromUrl();
 
@@ -85,15 +38,13 @@ export function initStore() {
         }
     }
 
-    store.goodsItems = loadDataStore();
-    store.filteredGoodsItems = filterGoods(goods, filters, getSearchStringFromUrl());
-    store.cart = cart;
-    store.sumCartItems = sumCartItems;
-    store.settings.isLSAvailabel = LS.isLSAvailabel();
+    store.filteredGoodsItems = filterGoods(store.goodsItems, filters, getSearchStringFromUrl());
+
+    LS.saveCartDataToLS({ '1': '2', '5': '2' }); //fake data
 
     // console.log(filters);
-    // console.log(getGoodsBrands(store.goodsItems), getGoodsCategorys(store.goodsItems));
-    // console.log(getGoodsBrands(store.filteredGoodsItems), getGoodsCategorys(store.filteredGoodsItems));
+    // console.log(getGoodsBrands(store.goodsItems), getGoodsCategories(store.goodsItems));
+    // console.log(getGoodsBrands(store.filteredGoodsItems), getGoodsCategories(store.filteredGoodsItems));
     // console.log(getCountGoodsByFieldName(store.goodsItems, 'brand', 'msi'));
     // console.log(getCountGoodsByFieldName(store.filteredGoodsItems, 'brand', 'msi'));
     // console.log(getMinMaxByFieldName(store.goodsItems, 'price'));
