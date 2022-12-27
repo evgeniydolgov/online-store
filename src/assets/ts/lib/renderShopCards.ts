10849ce35f4dd702a06e51047284962acd539849
Dudarik <Dudarik@yandex.ru>
2022-12-27 11:01:14 +0500

    refactor: render soping cards

---
+++ src/assets/ts/lib/renderShopCards.ts
@@ -0,0 +1,16 @@
+import { store } from '../store';
+import { getCardHtml } from './getCardHtml';
+
+export const renderShopCards = async (goodsRenderId: string) => {
+    const goodsDiv = document.querySelector(goodsRenderId);
+
+    const view = store.view_settings.mode;
+
+    const goodsCardsHtmlArr: HTMLElement[] = [];
+    console.log('render cards');
+
+    for (let i = 0; i < store.filteredGoodsItems.length; i++)
+        goodsCardsHtmlArr.push(await getCardHtml(store.filteredGoodsItems[i], { view }));
+
+    if (goodsDiv) goodsDiv.append(...goodsCardsHtmlArr);
+};
--- src/assets/ts/lib/renderStore.ts
+++ src/assets/ts/lib/renderStore.ts
@@ -1,9 +1,10 @@
 import { store } from '../store';
 import { checkElem } from '../helpers/checkers';
 // import { dualRangeSlider } from '../helpers/slide_finctions';
-import { getCardHtml } from './getCardHtml';
+
 import { handlerViewSwitch } from './handlers';
 import { getValueFilterHtml } from './getValueFilterHtml';
+import { renderShopCards } from './renderShopCards';
 
 export async function renderStore() {
     console.log(store);
@@ -23,24 +24,21 @@ export async function renderStore() {
 
     const view = store.view_settings.mode;
 
-    const goodsCardsHtmlArr: HTMLElement[] = [];
-
     const viewSwithcer = checkElem(newPage.querySelector(`#radio-${view}`));
 
     if (viewSwithcer instanceof HTMLInputElement) viewSwithcer.checked = true;
 
     newPage.querySelector('#select-shop-view')?.addEventListener('click', handlerViewSwitch);
 
-    for (let i = 0; i < store.filteredGoodsItems.length; i++)
-        goodsCardsHtmlArr.push(await getCardHtml(store.filteredGoodsItems[i], { view }));
-
-    const goodsDiv = checkElem(newPage).querySelector('#goods');
+    // for (let i = 0; i < store.filteredGoodsItems.length; i++)
+    //     goodsCardsHtmlArr.push(await getCardHtml(store.filteredGoodsItems[i], { view }));
 
     // render filters ===============================================================================
 
     const brand_filter = await getValueFilterHtml(store.filters_settings.all_brand, {
         filter_title: 'Брэнды',
         filter_name: 'brand',
+        filter_settings: store.filters_settings._brand,
         goods: store.goodsItems,
         filtredGoods: store.filteredGoodsItems,
     });
@@ -48,11 +46,12 @@ export async function renderStore() {
     const category_filter = await getValueFilterHtml(store.filters_settings.all_category, {
         filter_title: 'Категории',
         filter_name: 'category',
+        filter_settings: store.filters_settings._category,
         goods: store.goodsItems,
         filtredGoods: store.filteredGoodsItems,
     });
 
-    console.log(brand_filter);
+    // console.log(brand_filter);
     const filtersDiv = checkElem(newPage).querySelector('#shop_filters');
 
     const filtersArr: HTMLElement[] = [];
@@ -65,7 +64,6 @@ export async function renderStore() {
     // render filters ===============================================================================
 
     // console.log(brand_filter);
-    if (goodsDiv) goodsDiv.append(...goodsCardsHtmlArr);
 
     checkElem(
         checkElem(newPage).querySelector('#find_goods')
@@ -73,6 +71,7 @@ export async function renderStore() {
 
     app.innerHTML = '';
     app.append(checkElem(newPage));
+    await renderShopCards('#goods');
 
     // new dualRangeSlider(document.getElementById('range-slider-price') as HTMLElement, 'min_price', 'max_price');
     // new dualRangeSlider(document.getElementById('range-slider-stock') as HTMLElement, 'min_stock', 'max_stock');
