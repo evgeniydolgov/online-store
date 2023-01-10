8812564e6826799f4597b79c68c1e63f60c64d9e
Dudarik <Dudarik@yandex.ru>
2023-01-10 19:33:23 +0500

    fix: minor fixes

--- src/assets/ts/lib/renderShopCards.ts
+++ src/assets/ts/lib/renderShopCards.ts
@@ -25,6 +25,15 @@ export const renderShopCards = async (goodsRenderId: string) => {
 
     if (goodsDiv) {
         goodsDiv.innerHTML = '';
+        if (goodsCardsHtmlArr.length === 0) {
+            const nothingFindDiv = document.createElement('div');
+
+            nothingFindDiv.innerText =
+                'Совсем ничего не нашлось... Возможно, все видяшки раскупили майнеры... Эх, а помните времена были... Майнеров не было... Видяшки дешовые были... Попробуйте изменить настроки фильтра! Или поиска...';
+            nothingFindDiv.classList.add('nothing_find_class');
+
+            goodsDiv.append(nothingFindDiv);
+        }
         goodsDiv.append(...goodsCardsHtmlArr);
     }
 
